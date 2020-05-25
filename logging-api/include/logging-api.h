ed6e1223b75797c570ef424ccb3239cbea39f438
root <root@DESKTOP-DDVHU7S.localdomain>
2020-05-25 23:46:01 +0200

    first commit

---
+++ logging-api/include/logging-api.h
@@ -0,0 +1,59 @@
+﻿// logging-api.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+
+#define LOG_DEBUG (message)								   \
+	LogMessage(logging::severity::DEBUG).flush()<<message<<"\n";
+
+#define LOG_INFO (message)								   \
+	LogMessage(logging::severity::INFO).flush()<<message<<"\n";
+
+#define LOG_WARN (message)								   \
+	LogMessage(logging::severity::WARN).flush()<<message<<"\n";
+
+#define LOG_ERROR (message)								   \
+	LogMessage(logging::severity::ERROR).flush()<<message<<"\n";
+
+#define LOG_FATAL (message)								   \
+	LogMessage(logging::severity::FATAL).flush()<<message<<"\n";
+
+
+#include<sstream>
+namespace logging
+{
+typedef enum class severity
+{
+	DEBUG = 0,
+	INFO,
+	WARNING,
+	ERROR,
+	FATAL,
+	maxNumber
+} ;
+
+const char* _severities[(int) severity::maxNumber] =
+{
+	"DEBUG",
+	"INFO",
+	"WARNING",
+	"ERROR",
+	"FATAL"
+};
+
+class LogMessage : public std::stringstream
+{
+public:
+	severity _severity;
+
+	LogMessage(const severity& i_severity);
+	virtual ~LogMessage();
+
+};
+} // logging
+
+
---
+++ logging-api/src/logging-api.cpp
@@ -0,0 +1,34 @@
+﻿// logging-api.cpp : Defines the entry point for the application.
+//
+
+#include <logging-api.h>
+
+namespace logging
+{
+	LogMessage::LogMessage(const severity& i_severity) :
+		_severity(i_severity)
+	{
+		*this << "[" << _severities[(int) _severity] << " " << __FILE__ << " " << __LINE__ << "]: ";
+	}
+
+	LogMessage::~LogMessage()
+	{
+		switch (_severity)
+		{
+		case severity::DEBUG:
+		case severity::INFO:
+			fprintf(stdout, str().c_str());
+			break;
+		case severity::WARNING:
+		case severity::ERROR:
+		case severity::FATAL:
+			fprintf(stderr, str().c_str());
+			break;
+		default:
+			;
+		}
+	}
+}
+
+
+
